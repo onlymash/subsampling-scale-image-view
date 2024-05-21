33546fe897eb5b41f49bbd5d7804ba68a8e5dc1a
onlymash <fiepi.dev@gmail.com>
2024-05-21 20:20:55 +0800

    Update dependencies

--- sample/src/main/kotlin/com/davemorrissey/labs/subscaleview/test/BasicFeaturesActivity.kt
+++ sample/src/main/kotlin/com/davemorrissey/labs/subscaleview/test/BasicFeaturesActivity.kt
@@ -1,14 +1,17 @@
 package com.davemorrissey.labs.subscaleview.test
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.pages_activity.*
+import com.davemorrissey.labs.subscaleview.test.databinding.PagesActivityBinding
 
 class BasicFeaturesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pages_activity)
+        val binding = PagesActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         actionBar?.title = getString(R.string.basic_title)
-        imageView.setImage("/android_asset/sanmartino.jpg")
+        binding.imageView.setImage("/android_asset/sanmartino.jpg")
     }
 }
